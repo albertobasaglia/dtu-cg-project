dcbd2f324c0d461a87ce654d428fabd9cefd3bb9
Alberto Basaglia <alberto.basaglia17@gmail.com>
2024-12-04 10:50:00 +0100

    reset scene after exit

--- script.js
+++ script.js
@@ -74,6 +74,7 @@ async function init() {
         interactiveDiv.style.display = "block";
         inspectDiv.style.display = "none";
         animationMenu.style.display = "inline-block";
+        scene = defaultScene;
     });
 
 
